c341d5ee881d268e71b7d5aa24302c2e11903b8e
kim jung in <37010399+imguru-mooc@users.noreply.github.com>
2019-05-16 15:01:20 +0900

    Add files via upload

--- 04_day/placement_new.cpp
+++ 04_day/placement_new.cpp
@@ -1,19 +1,21 @@
 #if 1
 #include <stdio.h>
 #include <stdlib.h>
+#include <new> // placement new를 사용하려면 new 헤더를 include 해야한다. 
 class AAA
 {
+	int data;
 	public:
 		AAA(){printf("AAA::AAA()\n");}
 		~AAA(){printf("AAA::~AAA()\n");}
 };
 int main()
 {
-	void *temp = malloc( sizeof(AAA));
+	AAA *temp = (AAA*)malloc(sizeof(AAA));
 	AAA* p = new (temp) AAA(); // 이미 생성된 메모리를 객체로 사용하는 기술
 	                           // placement new
-	//p->~AAA();
-	//free(p);
+	p->~AAA();
+	free(p);
 	return 0;
 }
 #endif
